22c78ee3a311a4d64044567b53f5699bf13e65fe
Landon Smith <landjbs@gmail.com>
2020-05-09 18:28:17 -0400

    update_hyperparams

--- runner.py
+++ runner.py
@@ -5,12 +5,12 @@ from tqdm import trange
 import matplotlib.pyplot as plt
 
 g = Graph(36, type='Funnel', flag=5)
-m = Model(id=0, lr=0.01)
 
-sweep_vals = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1]
+sweep_vals = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1, 5, 10]
 final_losses = []
 
 for lr in sweep_vals:
+    m = Model(id=0, lr=lr)
     for _ in trange(100):
         x, y = next(iter(g.train_loader))
         m.step(x, y)
@@ -25,7 +25,7 @@ plt.ylabel('Validation Loss After 1000 Iterations')
 plt.savefig('lr_sweep')
 plt.show()
 
-# g.train(1000)
-# g.vis_global_params(root='test')
-# g.vis_individual_params(root='test')
-# g.vis_all_single_net(id=0, root='test')
+# g.train(100)
+# g.vis_global_params(root='shorter')
+# g.vis_individual_params(root='shorter')
+# g.vis_all_single_net(id=0, root='shorter')
