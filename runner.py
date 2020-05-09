d68502437b45957731ca643f0181962cd6b4d54c
Landon Smith <landjbs@gmail.com>
2020-05-09 17:44:41 -0400

    update_hyperparams

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ from base_graph import Graph
 
 
 x = Graph(36, type='Funnel', flag=5)
-x.train(100)
+x.train(1000)
 x.vis_global_params(root='test')
 x.vis_individual_params(root='test')
 x.vis_all_single_net(id=0, root='test')
