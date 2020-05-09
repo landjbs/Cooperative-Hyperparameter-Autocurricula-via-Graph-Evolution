0705c75a81702b0304f3b9bceb3a14a76d2c308d
Landon Smith <landjbs@gmail.com>
2020-05-09 18:23:42 -0400

    update_hyperparams

--- runner.py
+++ runner.py
@@ -2,17 +2,27 @@ from base_graph import Graph
 from model import Model
 
 from tqdm import trange
+import matplotlib.pyplot as plt
 
 g = Graph(36, type='Funnel', flag=5)
 m = Model(id=0, lr=0.01)
 
-for _ in trange(1000):
-    x, y = next(iter(g.train_loader))
-    m.step(x, y)
-    x, y = next(iter(g.eval_loader))
-    m.eval(x, y)
+sweep_vals = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1]
+final_losses = []
 
-plt.plot(m.param_logs['loss'])
+for lr in sweep_vals:
+    for _ in trange(100):
+        x, y = next(iter(g.train_loader))
+        m.step(x, y)
+        x, y = next(iter(g.eval_loader))
+        final_losses.append(m.eval(x, y))
+
+
+plt.plot(sweep_vals, final_losses)
+plt.xscale('log')
+plt.xlabel('Log Learning Rate')
+plt.ylabel('Validation Loss After 1000 Iterations')
+plt.savefig('lr_sweep')
 plt.show()
 
 # g.train(1000)
