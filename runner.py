e4d94082e3a06d9e1c5af92b38ef4600eb79c111
Landon Smith <landjbs@gmail.com>
2020-05-09 18:33:49 -0400

    lr sweep

--- runner.py
+++ runner.py
@@ -6,26 +6,26 @@ import matplotlib.pyplot as plt
 
 g = Graph(36, type='Funnel', flag=5)
 
-sweep_vals = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1, 5, 10]
-final_losses = []
+# sweep_vals = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 1, 5, 10]
+# final_losses = []
+#
+# for lr in sweep_vals:
+#     m = Model(id=0, lr=lr)
+#     for _ in trange(500):
+#         x, y = next(iter(g.train_loader))
+#         m.step(x, y)
+#         x, y = next(iter(g.eval_loader))
+#     final_losses.append(m.eval(x, y))
+#
+#
+# plt.plot(sweep_vals, final_losses)
+# plt.xscale('log')
+# plt.xlabel('Log Learning Rate')
+# plt.ylabel('Validation Loss After 1000 Iterations')
+# plt.savefig('lr_sweep')
+# plt.show()
 
-for lr in sweep_vals:
-    m = Model(id=0, lr=lr)
-    for _ in trange(500):
-        x, y = next(iter(g.train_loader))
-        m.step(x, y)
-        x, y = next(iter(g.eval_loader))
-    final_losses.append(m.eval(x, y))
-
-
-plt.plot(sweep_vals, final_losses)
-plt.xscale('log')
-plt.xlabel('Log Learning Rate')
-plt.ylabel('Validation Loss After 1000 Iterations')
-plt.savefig('lr_sweep')
-plt.show()
-
-# g.train(100)
-# g.vis_global_params(root='shorter')
-# g.vis_individual_params(root='shorter')
-# g.vis_all_single_net(id=0, root='shorter')
+g.train(500)
+g.vis_global_params(root='shorter')
+g.vis_individual_params(root='shorter')
+g.vis_all_single_net(id=0, root='shorter')
