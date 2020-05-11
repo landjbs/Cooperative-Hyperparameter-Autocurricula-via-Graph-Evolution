64377415620bdb0fe3f639304c5c90110a1662db
Harrison-Oatman <hroatman@college.harvard.edu>
2020-05-11 14:03:20 -0400

    results of moran experiment uploaded

--- moran.py
+++ moran.py
@@ -8,7 +8,7 @@ def run_trial(vertexFitnesses, adjMat, steps):
         N = len(vertexFitnesses)
         fitSum = float(sum(vertexFitnesses))
         p = [fit/fitSum for fit in vertexFitnesses]
-        reproducing = np.random.choice(range(N), size =10,p=p)
+        reproducing = np.random.choice(range(N), size =1,p=p)
         for parent in reproducing:
             p = adjMat[parent]
             if sum(p) > 0:
@@ -21,36 +21,39 @@ def run_trial(vertexFitnesses, adjMat, steps):
 
 r = 1.5
 trials = 1000
-max_steps = 1000
+max_steps = 50000
 
-for N, flag in [(7,2),(13,3),(21,4),(31,5),(43,6)]:
-    for type in ["Moran","Funnel","Superfan","Star"]:
-        print(N,type,flag)
-        if type == "Star":
-            adjMat,_ = generate_graph(N, "Funnel", N - 1)
-        else:
-            adjMat,_ = generate_graph(N, type, flag)
-        count = 0
-        for trial in range(trials):
-            vertexFitnesses = np.ones(N)
-            idx = np.random.choice(range(N))
-            vertexFitnesses[idx] = r
-            vertexFitnesses = run_trial(vertexFitnesses, adjMat, max_steps)
-            if np.median(vertexFitnesses) == r:
-                count += 0.001
-            print(vertexFitnesses)
-        if type == "Moran":
-            plt.scatter(N, count, c="k")
-        elif type == "Funnel":
-            plt.scatter(N, count, c="r")
-        elif type == "Star":
-            plt.scatter(N, count, c="y")
-        else:
-            plt.scatter(N, count, c="b")
-
-x = np.linspace(0,45)
+with open("moranresults.txt","w") as f:
+    for N, flag in [(7,2),(13,3),(21,4),(31,5)]:
+        for type in ["Moran","Funnel","Superfan","Star"]:
+            print(N,type,flag)
+            if type == "Star":
+                adjMat,_ = generate_graph(N, "Funnel", N - 1)
+            else:
+                adjMat,_ = generate_graph(N, type, flag)
+            count = 0
+            for trial in range(trials):
+                vertexFitnesses = np.ones(N)
+                idx = np.random.choice(range(N))
+                vertexFitnesses[idx] = r
+                vertexFitnesses = run_trial(vertexFitnesses, adjMat, max_steps)
+                if np.median(vertexFitnesses) == r:
+                    count += 0.001
+                print(vertexFitnesses)
+            if type == "Moran":
+                plt.scatter(N, count, c="k")
+            elif type == "Funnel":
+                plt.scatter(N, count, c="r")
+            elif type == "Star":
+                plt.scatter(N, count, c="y")
+            else:
+                plt.scatter(N, count, c="b")
+            f.write(str(f'{type,N,count}\n'))
+x = np.linspace(1,31)
 y = (1 - 1/r)/(1 - 1/(r**x))
+y2 = (1 - 1/(r**2))/(1 - 1/(r**(2*x)))
 plt.plot(x,y, c="k")
+plt.plot(x,y2, c="y")
 plt.xlabel("Population Size")
 plt.ylabel("Mutant Fixation Rate")
 plt.title("Fixation Rate of Mutatant r = 1.5")
