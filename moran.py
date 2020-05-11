026ee0769ee23f2a42b1f868335b23ea859cff17
Harrison-Oatman <hroatman@college.harvard.edu>
2020-05-10 22:56:18 -0400

    images generated

--- moran.py
+++ moran.py
@@ -8,22 +8,22 @@ def run_trial(vertexFitnesses, adjMat, steps):
         N = len(vertexFitnesses)
         fitSum = float(sum(vertexFitnesses))
         p = [fit/fitSum for fit in vertexFitnesses]
-        parent = np.random.choice(range(N),p=p)
-
-        p = adjMat[parent]
-        if sum(p) > 0:
-            child = np.random.choice(range(N), p=p)
-            vertexFitnesses[child] = vertexFitnesses[parent]
-        if step % 100 == 0:
-            if min(vertexFitnesses) == max(vertexFitnesses):
-                return vertexFitnesses
+        reproducing = np.random.choice(range(N), size =10,p=p)
+        for parent in reproducing:
+            p = adjMat[parent]
+            if sum(p) > 0:
+                child = np.random.choice(range(N), p=p)
+                vertexFitnesses[child] = vertexFitnesses[parent]
+            if step % 100 == 0:
+                if min(vertexFitnesses) == max(vertexFitnesses):
+                    return vertexFitnesses
     return vertexFitnesses
 
 r = 1.5
-trials = 100
-max_steps = 25000
+trials = 1000
+max_steps = 1000
 
-for N, flag in [(7,2),(13,3),(21,4),(31,5),(43,6),(111,10)]:
+for N, flag in [(7,2),(13,3),(21,4),(31,5),(43,6)]:
     for type in ["Moran","Funnel","Superfan","Star"]:
         print(N,type,flag)
         if type == "Star":
@@ -37,7 +37,7 @@ for N, flag in [(7,2),(13,3),(21,4),(31,5),(43,6),(111,10)]:
             vertexFitnesses[idx] = r
             vertexFitnesses = run_trial(vertexFitnesses, adjMat, max_steps)
             if np.median(vertexFitnesses) == r:
-                count += 0.01
+                count += 0.001
             print(vertexFitnesses)
         if type == "Moran":
             plt.scatter(N, count, c="k")
@@ -48,7 +48,7 @@ for N, flag in [(7,2),(13,3),(21,4),(31,5),(43,6),(111,10)]:
         else:
             plt.scatter(N, count, c="b")
 
-x = np.linspace(0,115)
+x = np.linspace(0,45)
 y = (1 - 1/r)/(1 - 1/(r**x))
 plt.plot(x,y, c="k")
 plt.xlabel("Population Size")
